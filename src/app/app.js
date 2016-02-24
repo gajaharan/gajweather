3198eafa51d388a5a31dd5fca52aab08551a18a2
gajaharan <gsatkunanandan@andigital.com>
2016-02-24 19:59:26 +0000

    Added routing so I can move the main forecast information from the index page to forecast page allowing different types of weather information data to be displayed e.g. current weather information.

--- src/app/app.js
+++ src/app/app.js
@@ -1,1 +1,6 @@
-var app = angular.module('gajWeatherApp', []);
+var app = angular.module('gajWeatherApp', ['ngRoute']);
+
+app.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/forecast', {templateUrl: 'app/components/forecast/forecast.html'});
+  $routeProvider.otherwise({redirectTo: '/forecast'});
+}]);
